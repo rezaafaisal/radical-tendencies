65f0fc090afeeee8720cefe0cbfc367a26844557
rezaafaisal <rezha.faizal567@gmail.com>
2023-07-01 14:21:43 +0800

    duplicate

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -9,8 +9,8 @@ export default function Home(props){
     return(
         <div>
             <div className="text-lg font-bold">{props.name} : {number}</div>
-            <button onClick={inrement} className="px-5 py-2 bg-teal-500 rounded text-white">Increment</button>
             <button onClick={decrement} className="px-5 py-2 bg-teal-500 rounded text-white">Decrement</button>
+            <button onClick={inrement} className="px-5 py-2 bg-teal-500 rounded text-white">Increment</button>
         </div>
     )
 }
