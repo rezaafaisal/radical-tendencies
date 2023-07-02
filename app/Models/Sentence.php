65f7a3a24457e81b736ba630c1d1495d72f13381
rezaafaisal <rezha.faizal567@gmail.com>
2023-07-02 08:26:50 +0800

    add mutator and cast

--- app/Models/Sentence.php
+++ app/Models/Sentence.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,4 +10,18 @@ use Illuminate\Database\Eloquent\Model;
 class Sentence extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = ['user_id', 'text', 'percentage', 'description'];
+
+    protected function text() : Attribute {
+        return Attribute::make(
+            set: fn (string $value) => strtolower($value),
+        );
+    }
+
+    protected function description() : Attribute{
+        return Attribute::make(
+            set: fn(string $value) => strtolower($value),
+        );
+    }
 }
