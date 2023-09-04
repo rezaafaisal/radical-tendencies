de6d34d7ca4ac84f25abddd2978bae467bef9bb5
rezaafaisal <rezha.faizal567@gmail.com>
2023-09-04 17:42:48 +0800

    on dashboard

--- resources/js/Layouts/Admin.jsx
+++ resources/js/Layouts/Admin.jsx
@@ -1,18 +1,21 @@
-import { faAnglesLeft, faBars, faChartPie, faQuoteLeft, faTrash, faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faAnglesLeft, faArrowRightFromBracket, faBars, faChartPie, faQuoteLeft, faTrash, faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "@inertiajs/react";
 import React, { useState } from "react";
 
-export default function AdminLayout({children}){
+export default function AdminLayout({children, active, title}){
 
     const [expanded, setExpanded] = useState(true)
     return(
         <div>
-            <header className="bg-white shadow fixed w-full top-0 h-20 flex items-center">
-                <div className="container mx-auto flex justify-between">
-                    <button onClick={()=>setExpanded(true)}>
-                        <FontAwesomeIcon icon={faBars} className="text-xl" />
-                    </button>
+            <nav className="bg-white shadow fixed w-full top-0 h-20 flex items-center">
+                <div className="w-full flex justify-between px-8">
+                    <div className="flex items-center ">
+                        <button onClick={()=>setExpanded(!expanded)} className="lg:hidden text-slate-600 h-10 w-10 flex justify-center items-center rounded-lg mr-3 duration-150 hover:bg-slate-200">
+                            <FontAwesomeIcon icon={faBars} className="text-xl" />
+                        </button>
+                        <span className="text-2xl block font-semibold text-cyan-500">Radikal Tes</span>
+                    </div>
                     <div className="relative group">
                         <div className="py-2">
                             <img src={'/avatar/user.jpg'} className="h-10 w-10 object-cover object-center rounded-full shadow cursor-pointer ring-offset-2 ring-2 ring-cyan-500"/>
@@ -36,53 +39,38 @@ export default function AdminLayout({children}){
                         </div>
                     </div>
                 </div>
-            </header>
-            {/* sidebar */}
-            <div className="flex justify-between">
-                <aside
-                    className={` top-0 left-0 h-screen bg-cyan-500 text-white duration-150 w-72 px-5 ${expanded?'translate-x-0':'-translate-x-full'}`}
-                >
-                    <div className="h-20 flex justify-between px-3 items-center">
-                        <Link className="text-2xl font-semibold">Radikal Tes</Link>
-                        <button onClick={()=>setExpanded(false)}>
-                            <FontAwesomeIcon
-                                icon={faAnglesLeft}
-                                className={`text-xl text-white/70 hover:text-white duration-150`}
-                            />
-                        </button>
-                    </div>
-                    <div className="mt-5 ">
-                        <div className="mb-5">
-                            <span className="text-xs text-white/70 font-semibold block p-3">BERANDA</span>
-                            <ul className="font-light">
-                                <li>
-                                    <Link className="p-3 hover:bg-cyan-300 hover:text-slate-600 duration-100 rounded-lg block">
-                                        <FontAwesomeIcon icon={faChartPie} className="mr-2 w-6" /> Dashboard
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="mb-5">
-                            <span className="text-xs text-white/70 font-semibold block p-3">PENGGUNA</span>
-                            <ul className="font-light">
-                                <li>
-                                    <Link className="p-3 hover:bg-cyan-300 hover:text-slate-600 duration-100 rounded-lg block">
-                                        <FontAwesomeIcon icon={faQuoteLeft} className="mr-2 w-6" /> Kalimat
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className="p-3 hover:bg-cyan-300 hover:text-slate-600 duration-100 rounded-lg block">
-                                        <FontAwesomeIcon icon={faUsers} className="mr-2 w-6" /> Pengguna
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </aside>
-                <main className="mt-20 w-max z-50 container mx-auto">
-                    {children}
-                </main>
-            </div>
+            </nav>
+            <aside className={`fixed top-0 mt-20 left-0 h-screen bg-white border duration-150 w-72 px-5 lg:translate-x-0 ${expanded ? 'sm:translate-x-0' : '-translate-x-full'}`}>
+                <div className="mt-10">
+                    <ul className="font-light flex  text-slate-600 flex-col gap-3">
+                        <li>
+                            <Link
+                                className={`p-3 duration-100 rounded-lg block ${active == 'dashboard' ? 'bg-cyan-500 text-white' : 'hover:bg-slate-200'}`}>
+                                <FontAwesomeIcon icon={faChartPie} className="mr-2 w-6" /> Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={`p-3 duration-100 rounded-lg block ${active == 'sentence' ? 'bg-cyan-500 text-white' : 'hover:bg-slate-200'}`}>
+                                <FontAwesomeIcon icon={faQuoteLeft} className="mr-2 w-6" /> Kalimat
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={`p-3 duration-100 rounded-lg block ${active == 'user' ? 'bg-cyan-500 text-white' : 'hover:bg-slate-200'}`}>
+                                <FontAwesomeIcon icon={faUsers} className="mr-2 w-6" /> Pengguna
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="p-3 duration-100 rounded-lg block hover:bg-slate-200">
+                                <FontAwesomeIcon icon={faArrowRightFromBracket} className="mr-2 w-6" /> Keluar
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+            <main className={`mt-20 z-50 lg:ml-72 p-8 text-slate-600`}>
+                <h1 className="font-semibold text-xl mb-10">{title}</h1>
+                {children}
+            </main>
         </div>
     )
 }
--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -1,14 +1,104 @@
 import React from "react";
 import AdminLayout from "../../Layouts/Admin";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCommentDots, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faFaceAngry, faFaceSmile } from "@fortawesome/free-regular-svg-icons";
 
 export default function Dashboard(){
     return (
-        <AdminLayout>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat quos, hic quidem molestiae nobis nam quae reiciendis nemo, excepturi illum itaque, asperiores eligendi distinctio repellat dolores nostrum adipisci sint!</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat quos, hic quidem molestiae nobis nam quae reiciendis nemo, excepturi illum itaque, asperiores eligendi distinctio repellat dolores nostrum adipisci sint!</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat quos, hic quidem molestiae nobis nam quae reiciendis nemo, excepturi illum itaque, asperiores eligendi distinctio repellat dolores nostrum adipisci sint!</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat quos, hic quidem molestiae nobis nam quae reiciendis nemo, excepturi illum itaque, asperiores eligendi distinctio repellat dolores nostrum adipisci sint!</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat quos, hic quidem molestiae nobis nam quae reiciendis nemo, excepturi illum itaque, asperiores eligendi distinctio repellat dolores nostrum adipisci sint!</p>
+        <AdminLayout active="dashboard" title="Dashboard" >
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-10">
+                <div className="p-5 bg-white rounded-lg border flex items-center gap-3">
+                    <div className="h-10 w-10 bg-blue-100 rounded-full text-blue-500 flex items-center justify-center">
+                        <FontAwesomeIcon icon={faUsers} />
+                    </div>
+                    <div>
+                        <span className="block text-sm font-light mb-2">Total Pengguna</span>
+                        <span className="block font-semibold text-lg">400</span>
+                    </div>
+                </div>
+                <div className="p-5 bg-white rounded-lg border flex items-center gap-3">
+                    <div className="h-10 w-10 bg-amber-100 rounded-full text-amber-500 flex items-center justify-center">
+                        <FontAwesomeIcon icon={faCommentDots} />
+                    </div>
+                    <div>
+                        <span className="block text-sm font-light mb-2">Total Kalimat</span>
+                        <span className="block font-semibold text-lg">400</span>
+                    </div>
+                </div>
+                <div className="p-5 bg-white rounded-lg border flex items-center gap-3">
+                    <div className="h-10 w-10 bg-green-100 rounded-full text-green-500 flex items-center justify-center">
+                        <FontAwesomeIcon icon={faFaceSmile} />
+                    </div>
+                    <div>
+                        <span className="block text-sm font-light mb-2">Total Tidak Radikal</span>
+                        <span className="block font-semibold text-lg">400</span>
+                    </div>
+                </div>
+                <div className="p-5 bg-white rounded-lg border flex items-center gap-3">
+                    <div className="h-10 w-10 bg-rose-100 rounded-full text-rose-500 flex items-center justify-center">
+                        <FontAwesomeIcon icon={faFaceAngry} />
+                    </div>
+                    <div>
+                        <span className="block text-sm font-light mb-2">Total Cenderung Radikal</span>
+                        <span className="block font-semibold text-lg">400</span>
+                    </div>
+                </div>
+            </div>
+            <div className="rounded-lg border bg-white overflow-hidden">
+                <table className="table-fixed divide-y w-full">
+                    <thead>
+                        <tr className="bg-slate-200">
+                            <th className="text-sm text-start font-light py-3 px-6 w-3/12">Pengguna</th>
+                            <th className="text-sm text-start font-light py-3 px-6 w-5/12">Kalimat</th>
+                            <th className="text-sm text-start font-light py-3 px-6 w-2/12">Prediksi</th>
+                            <th className="text-sm text-start font-light py-3 px-6 w-2/12">Tanggal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="hover:bg-slate-100 duration-100">
+                            <td className="text-sm font-light py-3 px-6">
+                                <div className="flex items-center gap-2">
+                                    <img src="https://api.slingacademy.com/public/sample-photos/1.jpeg" alt="" className="w-10 h-10 object-cover object-center rounded-full" />
+                                    <div>
+                                        <span className="block font-semibold">Burung Hantu</span>
+                                        <span className="text-xs font-light">burung****@gmail.com</span>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="text-sm font-light py-3 px-6">
+                                <p className="text-xs text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis nam aperiam nostrum. Voluptatem possimus repudiandae cupiditate optio iusto! Reiciendis velit harum ratione ex ad id quisquam? A corporis in eveniet?</p>
+                            </td>
+                            <td className="text-sm font-light py-3 px-6">
+                                <span className="block text-xs bg-teal-100 text-teal-500 text-center p-2 rounded-full">Tidak Radikal</span>
+                            </td>
+                            <td className="text-sm font-light py-3 px-6">
+                                02/12/2023 24:00
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-100 duration-100">
+                            <td className="text-sm font-light py-3 px-6">
+                                <div className="flex items-center gap-2">
+                                    <img src="https://api.slingacademy.com/public/sample-photos/1.jpeg" alt="" className="w-10 h-10 object-cover object-center rounded-full" />
+                                    <div>
+                                        <span className="block font-semibold">Burung Hantu</span>
+                                        <span className="text-xs font-light">burung****@gmail.com</span>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="text-sm font-light py-3 px-6">
+                                <p className="text-xs text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis nam aperiam nostrum. Voluptatem possimus repudiandae cupiditate optio iusto! Reiciendis velit harum ratione ex ad id quisquam? A corporis in eveniet?</p>
+                            </td>
+                            <td className="text-sm font-light py-3 px-6">
+                                <span className="block text-xs bg-rose-100 text-rose-500 text-center p-2 rounded-full">Cenderung Radikal</span>
+                            </td>
+                            <td className="text-sm font-light py-3 px-6">
+                                02/12/2023 24:00
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </AdminLayout>
     )
 }
