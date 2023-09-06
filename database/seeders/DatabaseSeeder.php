98da5b18fad07c5afed986e13fbd09f8dc8cb008
rezaafaisal <rezha.faizal567@gmail.com>
2023-09-06 22:47:46 +0800

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,21 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(50)->has(
-        //     Sentence::factory(random_int(10, 50))
-        // )->create();
+        User::factory(50)->has(
+            Sentence::factory(random_int(10, 50))
+        )->create();
 
         User::create([
-                'name' => 'Andi Engku Putribuana',
-                'email' => 'test@example.com',
+                'name' => 'Admin Putri',
+                'email' => 'admin@gmail.com',
                 'avatar' => 'user.jpg',
                 'level' => 'admin',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
             ],
         );
         User::create([
-                'name' => 'Jake Sully',
-                'email' => 'jake@example.com',
+                'name' => 'Putri Buana',
+                'email' => 'putri@gmail.com',
                 'avatar' => 'user.jpg',
                 'level' => 'user',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
